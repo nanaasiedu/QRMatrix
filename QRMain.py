cdcc9aba853c28cdb0693ec3c05538d093231b28
Nana Asiedu-Ampem <na1814@ic.ac.uk>
2016-03-12 18:18:42 +0000

    Added QR logic

--- QRMain.py
+++ QRMain.py
@@ -3,5 +3,14 @@ import QRMatrix as QR
 print "=== Nana's QR decomposition ==="
 dim = input("Please enter dimensions you would like to use for the matrix A : ")
 
-matrixA = QR.generateSymMatrix(dim)
-QR.printMatrix(matrixA)
+A = QR.t1 #QR.generateSymMatrix(dim)
+QR.printMatrix("Matrix A =", A)
+
+Q, R = QR.gram_schmidt(A)
+QR.printMatrix("Initial Matrix Q =", Q)
+QR.printMatrix("Initial Matrix R =", R)
+
+eigenvalues, eigenvectors = QR.QRIteration(A)
+
+QR.printMatrix("eigenvalues =", eigenvalues)
+QR.printMatrix("eigenvectors =", eigenvectors)
--- QRMatrix.py
+++ QRMatrix.py
@@ -1,11 +1,20 @@
 from random import random, randint
-#from numpy.linalg import
+import numpy as np
+from math import fabs
 
 elemMax = 7 # Maximum matrix element
+epsilon = 0.0000000001 # difference limit 10^-10
+
+#Test matrices
+t1 =  np.zeros((2, 2))
+t1[0][0] = 0
+t1[0][1] = 1
+t1[1][0] = -2
+t1[1][1] = -3
 
 # Generates random matrix element
 def randElem():
-    return randint(0,elemMax) #random()*elemMax ================================
+    return random()*elemMax
 
 # Returns the position opposite to (i,j) in the matrix
 def opPos(i, j):
@@ -13,10 +22,10 @@ def opPos(i, j):
 
 # Generates a random symmetric matrix
 def generateSymMatrix(dim):
-    matrix = [[0 for x in range(dim)] for x in range(dim)]
+    matrix = np.zeros((dim, dim))
 
-    for j in range (0, dim):
-        for i in range(0, dim):
+    for j in range (dim):
+        for i in range(dim):
             if i >= j:
                 matrix[j][i] = randElem()
             else:
@@ -25,6 +34,67 @@ def generateSymMatrix(dim):
 
     return matrix
 
-def printMatrix(matrix):
-    for r in matrix:
-        print r
+def printMatrix(msg, matrix):
+    print msg
+
+    print matrix
+
+    print ""
+
+def gram_schmidt(A):
+	n = np.shape(A)[1]
+
+	Q =  np.zeros((n, n))
+	R =  np.zeros((n, n))
+
+	for j in range(n):
+
+		v = A[:,j]
+
+		for i in range(j):
+
+			R[i,j] = dotProduct(Q[:,i], A[:,j])
+
+			v = v.squeeze() - (R[i,j] * Q[:,i])
+
+		R[j,j] =  np.linalg.norm(v)
+		Q[:,j] = (v / R[j,j]).squeeze()
+
+	return Q, R
+
+def dotProduct(A, B):
+    n = np.shape(A)[0]
+
+    total = 0
+
+    for i in range(n):
+        total += A[i] * B[i]
+
+    return total
+
+# Returns diagonal of a square matrix
+def getDiag(A):
+    diag = []
+
+    for i in range(A.shape[0]):
+        diag.append(A[i][i])
+
+    return diag
+
+def QRIteration(A):
+    Q, R = gram_schmidt(A)
+
+    A2 = R.dot(Q)
+
+    evectors = Q
+
+    oldQ = Q
+    while fabs(A[0][0] - A2[0][0]) >= epsilon:
+        A = A2
+        Q, R = gram_schmidt(A)
+        A2 = R.dot(Q)
+
+        evectors = oldQ.dot(Q)
+        oldQ = evectors
+
+    return getDiag(A2), evectors
